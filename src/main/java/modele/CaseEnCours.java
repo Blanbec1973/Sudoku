0dcc8a6dfef059ba0a28da74041d0d6cd6363bdb
Blanbec1973 <richard.heyner@gmail.com>
2020-12-01 17:38:27 +0100

    Cleaning.

--- src/main/java/modele/CaseEnCours.java
+++ src/main/java/modele/CaseEnCours.java
@@ -3,14 +3,12 @@ package modele;
 public class CaseEnCours {
 	private static int xSearch, ySearch, numCase, xRegion, yRegion ;
 	
-
 	public static int getXSearch() {return xSearch;}
 	public static int getYSearch() {return ySearch;}
 	public static int getNumCase() {return numCase;}
 	public static int getxRegion() {return xRegion;}
 	public static int getyRegion() {return yRegion;}
 	public static void setCaseEnCours(int numCase) {
-        //Calcule xSearch et ySearch à partir de numCase
         xSearch = Utils.calculXsearch(numCase);
         ySearch = Utils.calculYsearch(numCase);
         CaseEnCours.setXYRegion();
@@ -21,9 +19,7 @@ public class CaseEnCours {
 		ySearch =y;
 		numCase = Utils.calculNumCase(x, y);
 	}
-	private static void setXYRegion() {
-        // Calcul de la région : 
-        
+	private static void setXYRegion() {        
         switch (xSearch) {
             case 0,1,2 -> xRegion=0;
             case 3,4,5 -> xRegion=3;
@@ -35,5 +31,4 @@ public class CaseEnCours {
             case 6,7,8 -> yRegion=6;
         }
     }
-
 }
