a3674d2bf3829b50ae576b2a6884381b6478c59a
Blanbec1973 <richard.heyner@gmail.com>
2020-12-06 19:19:01 +0100

    Cleaning.

--- src/main/java/modele/InitialiseurDeGrille.java
+++ src/main/java/modele/InitialiseurDeGrille.java
@@ -4,8 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class InitialiseurDeGrille {
 	private Grille grille;
