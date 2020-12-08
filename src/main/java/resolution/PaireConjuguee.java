177a75050b7f6a081bfc0eabdf83fc0d7c7bbb09
Blanbec1973 <richard.heyner@gmail.com>
2020-12-08 21:09:09 +0100

    Cleaning factorisation.

---
+++ src/main/java/resolution/PaireConjuguee.java
@@ -0,0 +1,36 @@
+package resolution;
+
+import modele.Grille;
+import modele.Modele;
+import modele.Utils;
+
+public abstract class PaireConjuguee extends MethodeResolution {
+
+	public PaireConjuguee(Modele modele, Grille grille) {
+		super(modele,grille);
+	}
+
+	@Override
+	public boolean traiteCaseEnCours(boolean goPourChangement) {
+		if (grille.getCaseEnCours().getNombreCandidats() != 2) return false;
+		
+		boolean trouve = this.detecteConfiguration();
+		if (!trouve) return false;
+
+		c1 = Utils.trouveCandidatNumero(grille.getCaseEnCours().getCandidats(), 1);
+        c2 = Utils.trouveCandidatNumero(grille.getCaseEnCours().getCandidats(), 2);
+		trouve = this.detecteCandidatAEliminer();
+		if (!trouve) return false;
+        
+        if (goPourChangement) {
+        	this.elimineCandidatCase(candidatAEliminer, xAction, yAction);
+        }
+        else {
+			modele.getControle().demandeHighlightCase(xAction, yAction);
+        }
+        return true;
+	}
+	
+	protected abstract boolean detecteConfiguration();
+	protected abstract boolean detecteCandidatAEliminer();
+}
