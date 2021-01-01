640c3ef684e1d8737fc1ee1f722fb7e3079d2de9
Blanbec1973 <richard.heyner@gmail.com>
2021-01-01 19:24:05 +0100

    Nouveau TU

---
+++ src/test/java/resolution/AbsenceCandidatEnLigneDansLesAutresRegionsTest.java
@@ -0,0 +1,51 @@
+package resolution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import modele.CaseEnCours;
+import modele.Grille;
+import modele.InitialiseurDeGrille;
+
+class AbsenceCandidatEnLigneDansLesAutresRegionsTest {
+
+	private static Grille grille;
+	private static AbsenceCandidatEnLigneDansLesAutresRegions methode;
+	
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		grille = new Grille();
+		InitialiseurDeGrille i = new InitialiseurDeGrille(grille);
+		i.init("C:\\Users\\heynerr\\Documents\\W-Workspace\\Sudoku\\src\\test\\resources\\grillesTest\\AbsenceCandidatEnLigneDansLesAutresRegions.sud");
+		i.calculTousLesCandidats();
+		methode = new AbsenceCandidatEnLigneDansLesAutresRegions(null, grille);
+	}
+
+	@Test
+	void testTraiteCaseEnCours() {
+		CaseEnCours.setCaseEnCours(76);
+		assertTrue(methode.traiteCaseEnCours(false));
+		assertEquals(67,methode.numCaseAction);
+	}
+	
+	@Test
+	void testTestCase() {
+		CaseEnCours.setCaseEnCours(76);
+		assertFalse(methode.testCase(0, 6));
+		assertTrue(methode.testCase(0, 3));
+	}
+	
+	@Test
+	void testDetecteCandidatAEliminer() {
+		CaseEnCours.setCaseEnCours(76);
+		methode.candidatAEliminer = 3;
+		assertTrue(methode.detecteCandidatAEliminer());
+		assertEquals(3,methode.xAction);
+		assertEquals(7,methode.yAction);
+	}
+
+}
