2357504a1f0bbb10cb0535ab7db36d4006089f9a
Blanbec1973 <richard.heyner@gmail.com>
2021-12-24 20:13:29 +0100

    Cleaning Sonar.

--- src/main/java/modele/MesProprietes.java
+++ src/main/java/modele/MesProprietes.java
@@ -1,12 +1,17 @@
 package modele;
 
+import controleur.Controle;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class MesProprietes {
     Properties prop = new Properties();
+    private static final Logger LOGGER = Logger.getLogger(Controle.class.getPackage().getName());
 
     public MesProprietes() {
         try (InputStream input = new FileInputStream(System.getProperty("user.dir")+"/src/main/resources/config.properties")) {
@@ -15,7 +20,7 @@ public class MesProprietes {
             prop.load(input);
 
         } catch (IOException ex) {
-            ex.printStackTrace();
+            LOGGER.log(Level.SEVERE,"Exception : {0}",ex.getMessage());
         }
     }
 
