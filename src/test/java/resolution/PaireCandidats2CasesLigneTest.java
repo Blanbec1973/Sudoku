aafdb59c6730ea0fbbcef0798eb7533e87c2ad84
Blanbec1973 <richard.heyner@gmail.com>
2024-12-23 14:27:14 +0100

    Increase coverage of PaireCandidats2CasesLigneTest

--- src/test/java/resolution/PaireCandidats2CasesLigneTest.java
+++ src/test/java/resolution/PaireCandidats2CasesLigneTest.java
@@ -1,5 +1,6 @@
 package resolution;
 
+import model.grille.CaseEnCours;
 import model.grille.Grille;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,42 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class PaireCandidats2CasesLigneTest {
     private PaireCandidats2CasesLigne methode;
+    private Grille grille;
 
     @BeforeEach
     void setUp() {
-        Grille grille = new Grille();
-        methode = new PaireCandidats2CasesLigne(null, grille);
+        grille = new Grille();
+        grille.init(System.getProperty("user.dir")+"/src/test/resources/grillesTest/PaireCandidats2CasesLigne.sud");
+        methode = new PaireCandidats2CasesLigne(null,grille);
+    }
+
+    @Test
+    void testTraiteCaseEnCours() {
+        CaseEnCours.setCaseEnCours(1);
+        assertFalse(methode.traiteCaseEnCours(false));
+        CaseEnCours.setCaseEnCours(64);
+        assertTrue(methode.traiteCaseEnCours(false));
+        assertEquals(64, methode.numCaseAction);
+        assertEquals(3, methode.candidatAEliminer);
+        grille.elimineCandidat(64,3);
+
+        assertTrue(methode.traiteCaseEnCours(false));
+        assertEquals(64, methode.numCaseAction);
+        assertEquals(8, methode.candidatAEliminer);
+        grille.elimineCandidat(64,8);
+
+        CaseEnCours.setCaseEnCours(65);
+        assertTrue(methode.traiteCaseEnCours(false));
+        assertEquals(65, methode.numCaseAction);
+        assertEquals(3, methode.candidatAEliminer);
+        grille.elimineCandidat(65,3);
+
+        assertTrue(methode.traiteCaseEnCours(false));
+        assertEquals(65, methode.numCaseAction);
+        assertEquals(8, methode.candidatAEliminer);
+        grille.elimineCandidat(65,8);
+
+        assertFalse(methode.traiteCaseEnCours(false));
     }
 
     @Test
