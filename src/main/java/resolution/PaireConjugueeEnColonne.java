218ba2f600c4cd4511c7eda06f8de11e80d3a480
Blanbec1973 <richard.heyner@gmail.com>
2025-11-14 13:17:49 +0100

    Suppression variables d'état dans MethodeResolution partie 10

--- src/main/java/resolution/PaireConjugueeEnColonne.java
+++ src/main/java/resolution/PaireConjugueeEnColonne.java
@@ -1,49 +1,103 @@
 package resolution;
 
+import model.grille.CandidatsCase;
 import model.grille.CaseContext;
 import model.grille.Grille;
+import utils.Utils;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-public class PaireConjugueeEnColonne extends PaireConjuguee {
+public class PaireConjugueeEnColonne extends MethodeResolution {
 
 	public PaireConjugueeEnColonne(Grille grille) {
 		super(grille);
 	}
 
-	protected boolean detecteConfiguration(CaseContext context) {
-		for (y2=0;y2<9;y2++) {
-            if (y2 != context.getY() &&
-                grille.isCaseATrouver(context.getX(),y2) &&
-                Arrays.equals(grille.getCandidatsTabBoolean(context.getNumCase()),
-                		grille.getCandidatsTabBoolean(context.getX(),y2))) {
-            	return true;
-            }
-        }
-		return false;
-	}
-	
-	protected Optional<ResolutionAction> detecteCandidatAEliminer(CaseContext context, int[] candidatsUtilises) {
-		int c1=candidatsUtilises[0];
-		int c2 = candidatsUtilises[1];
-		for (int yAction=0;yAction<9;yAction++) {
-        	if (grille.isCaseATrouver(context.getX(),yAction) &&
-        			yAction!=context.getY() && yAction !=y2) {
-        		if (grille.isCandidat(context.getX(),yAction, c1) ) {
-        			return Optional.of(creerResolutionAction(context.getX(),
-							                                 yAction,
-							                                 c1,
-							                                 context, candidatsUtilises));
-        		}
-        		if (grille.isCandidat(context.getX(),yAction, c2) ) {
-					return Optional.of(creerResolutionAction(context.getX(),
-							yAction,
-							c2,
-							context, candidatsUtilises));
-        		}
-            }
-        }
+	@Override
+	public Optional<ResolutionAction> traiteCaseEnCours(CaseContext context, boolean goPourChangement) {
+		if (grilleService.calculNombreCaseATrouverDansColonne(context.getX()) <= 2) return Optional.empty();
+
+		List<int[]> paires = trouverPairesDansColonne(context);
+
+		for (int[] paire : paires) {
+			Optional<PaireResult> paireOpt = calculerPaire(context, paire[0]);
+			if (paireOpt.isPresent()) {
+				PaireResult paireResult = paireOpt.get();
+
+				Optional<ActionCandidate> actionOpt = detecteCandidatAEliminer(context, paireResult);
+				if (actionOpt.isPresent()) {
+					return Optional.of(creerResolutionAction(context, paireResult, actionOpt.get()));
+				}
+			}
+		}
 		return Optional.empty();
-	}	
+	}
+
+	private List<int[]> trouverPairesDansColonne(CaseContext context) {
+		List<int[]> paires = new ArrayList<>();
+		int x = context.getX();
+		int y = context.getY();
+
+		for (int y2 = 0; y2 < 9; y2++) {
+			if (y2 != y && grille.isCaseATrouver(x, y2)) {
+				paires.add(new int[]{y2});
+			}
+		}
+		return paires;
+	}
+
+	private Optional<PaireResult> calculerPaire(CaseContext context, int y2) {
+		int x = context.getX();
+
+		CandidatsCase testCandidats = new CandidatsCase();
+		testCandidats.setCandidats(Utils.calculOuLogique2Candidats(
+				grille.getCandidatsTabBoolean(context.getNumCase()),
+				grille.getCandidatsTabBoolean(x, y2)
+		));
+
+		if (testCandidats.getNombreCandidats() != 2) return Optional.empty();
+
+		int c1 = Utils.trouveCandidatNumero(testCandidats, 1);
+		int c2 = Utils.trouveCandidatNumero(testCandidats, 2);
+
+		return Optional.of(new PaireResult(c1, c2, y2));
+	}
+
+	private Optional<ActionCandidate> detecteCandidatAEliminer(CaseContext context, PaireResult paireResult) {
+		int x = context.getX();
+
+		for (int ord = 0; ord < 9; ord++) {
+			if (grille.isCaseATrouver(x, ord) && ord != context.getY() && ord != paireResult.y2) {
+				if (grille.isCandidat(x, ord, paireResult.c1)) return Optional.of(new ActionCandidate(x, ord, paireResult.c1));
+				if (grille.isCandidat(x, ord, paireResult.c2)) return Optional.of(new ActionCandidate(x, ord, paireResult.c2));
+			}
+		}
+		return Optional.empty();
+	}
+
+	private ResolutionAction creerResolutionAction(CaseContext context, PaireResult paireResult, ActionCandidate action) {
+		int numCaseAction = Grille.calculNumCase(action.xAction, action.yAction);
+		int[] candidatsUtilises = { paireResult.c1, paireResult.c2 };
+		return new ResolutionAction(numCaseAction, null, action.candidatAEliminer, this, context, candidatsUtilises);
+	}
+
+	private static class PaireResult {
+		int c1;
+		int c2;
+		int y2;
+		PaireResult(int c1, int c2, int y2) {
+			this.c1 = c1; this.c2 = c2; this.y2 = y2;
+		}
+	}
+
+	private static class ActionCandidate {
+		int xAction;
+		int yAction;
+		int candidatAEliminer;
+		ActionCandidate(int xAction, int yAction, int candidatAEliminer) {
+			this.xAction = xAction; this.yAction = yAction; this.candidatAEliminer = candidatAEliminer;
+		}
+	}
 }
--- src/test/java/resolution/PaireConjugueeEnColonneTest.java
+++ src/test/java/resolution/PaireConjugueeEnColonneTest.java
@@ -9,7 +9,6 @@ import java.nio.file.Paths;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PaireConjugueeEnColonneTest {
 	private static PaireConjugueeEnColonne methode;
 	private final Grille grille = new Grille();
@@ -22,81 +21,59 @@ class PaireConjugueeEnColonneTest {
 	}
 
 	@Test
-	@Order(1)
 	void testTraiteCaseEnCours() {
 		CaseContext context = new CaseContext(18);
 		assertFalse(methode.traiteCaseEnCours(context, false).isPresent());
+
 		CaseContext context2 = new CaseContext(36);
-		//assertTrue(methode.traiteCaseEnCours(context2, false).isPresent());
 		ResolutionAction action = methode.traiteCaseEnCours(context2, false)
 				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(3,action.getCandidatUtilise(0));
 		assertEquals(7,action.getCandidatUtilise(1));
-	}
-
-	@Test
-	@Order(2)
-	void testDetecteConfiguration() {
-		CaseContext context = new CaseContext(2);
-		assertFalse(methode.detecteConfiguration(context));
-		CaseContext context2 = new CaseContext(36);
-		assertTrue(methode.detecteConfiguration(context2));
-	}
-	
-	@Test
-	@Order(3)
-	void testDetecteCandidatAEliminer() {
-		CaseContext context = new CaseContext(36);
-		int[] candidatsUtilises = {3, 7};
-		methode.y2=4;
-
-		ResolutionAction action = methode.detecteCandidatAEliminer(context, candidatsUtilises)
-				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(7, action.getCandidatAEliminer());
 		assertEquals(18,action.getNumCaseAction());
 		grille.elimineCandidat(18,7);
 
-		ResolutionAction action2 = methode.detecteCandidatAEliminer(context, candidatsUtilises)
+		ResolutionAction action2 = methode.traiteCaseEnCours(context2, false)
 				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(7, action2.getCandidatAEliminer());
 		assertEquals(27,action2.getNumCaseAction());
 		grille.elimineCandidat(27,7);
 
-		ResolutionAction action3 = methode.detecteCandidatAEliminer(context, candidatsUtilises)
+		ResolutionAction action3 = methode.traiteCaseEnCours(context2, false)
 				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(7, action3.getCandidatAEliminer());
 		assertEquals(54,action3.getNumCaseAction());
 		grille.elimineCandidat(54,7);
 
-		ResolutionAction action4 = methode.detecteCandidatAEliminer(context, candidatsUtilises)
+		ResolutionAction action4 = methode.traiteCaseEnCours(context2, false)
 				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(3, action4.getCandidatAEliminer());
 		assertEquals(63,action4.getNumCaseAction());
 		grille.elimineCandidat(63,3);
 
-		ResolutionAction action5 = methode.detecteCandidatAEliminer(context, candidatsUtilises)
+		ResolutionAction action5 = methode.traiteCaseEnCours(context2, false)
 				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(7, action5.getCandidatAEliminer());
 		assertEquals(63,action5.getNumCaseAction());
 		grille.elimineCandidat(63,7);
 
-		ResolutionAction action6 = methode.detecteCandidatAEliminer(context, candidatsUtilises)
+		ResolutionAction action6 = methode.traiteCaseEnCours(context2, false)
 				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(3, action6.getCandidatAEliminer());
 		assertEquals(72,action6.getNumCaseAction());
 		grille.elimineCandidat(72,3);
 
-		ResolutionAction action7 = methode.detecteCandidatAEliminer(context, candidatsUtilises)
+		ResolutionAction action7 = methode.traiteCaseEnCours(context2, false)
 				.orElseThrow(()->new AssertionError("Should be present"));
 		assertEquals(7, action7.getCandidatAEliminer());
 		assertEquals(72,action7.getNumCaseAction());
 		grille.elimineCandidat(72,7);
 
-		assertFalse(methode.detecteCandidatAEliminer(context, candidatsUtilises).isPresent());
+		assertFalse(methode.traiteCaseEnCours(context2, false).isPresent());
 	}
 
 	@Test
-	@Order(4)
 	void testGetSimpleName() {
 		assertEquals("PaireConjugueeEnColonne",methode.getSimpleName());
 	}
