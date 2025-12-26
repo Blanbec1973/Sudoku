b98e1e7e1de2b4f608f8f24fdb6b7cc6f271524d
HEYNER Richard <richard.heyner@gmail.com>
2025-12-26 22:46:35 +0100

    Upgrade dependencies.

--- src/test/java/dojo/RunCucumberTest.java
+++ src/test/java/dojo/RunCucumberTest.java
@@ -1,13 +1,19 @@
 package dojo;
 
-import io.cucumber.junit.CucumberOptions;
-import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources",
-        plugin = { "pretty" },
-        monochrome = true
-)
-public class RunCucumberTest { }
+
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "dojo")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
+
